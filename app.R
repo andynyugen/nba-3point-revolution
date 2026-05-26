3fa87657cf4ec146c74195d8f78b7e9e00e2b031
Andy Nguyen <andynguyena49@gmail.com>
2026-05-26 00:51:52 -0700

    Incorporated RDS usage in app

--- app.R
+++ app.R
@@ -1,16 +1,8 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(bslib)
 library(hoopR)
 library(tidyverse)
+library(rsconnect)
 
 # Load datasets
 three_point_trend       <- readRDS("three_point_trend.rds")
